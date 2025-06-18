ca74181564e45172842f743c739afda91d10d7a8
Matthew Sam <matthewsampanjaitan@gmail.com>
2025-06-18 16:09:56 +0700

    complete the about me section

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 document.getElementById('scrollToAbout').addEventListener('click', function() {
-  document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+  document.getElementById('about-me').scrollIntoView({ behavior: 'smooth' });
 });
 
 window.addEventListener('scroll', function() {
   const scrollTrigger = 300; // Adjust as needed
-  //const homeBg = document.querySelector('.home-bg');
   const aboutBg = document.querySelector('.about-bg');
   const waveBg = document.querySelector('.wave');
   const colorLogo = document.querySelector('.header-nav');
   const colorLinks = document.querySelectorAll('.nav-links a'); // Select all links
+  const aboutContent = document.querySelector('.about-content');
+  const scrollTrigger2 = 400;
   if (window.scrollY > scrollTrigger) {
     waveBg.style.opacity = 0;
     aboutBg.style.opacity = 1;
@@ -16,6 +17,7 @@ window.addEventListener('scroll', function() {
     colorLinks.forEach(link => {
       link.style.color = '#f5f5f5';
     });
+    aboutBg.classList.add('visible');
   } else {
     waveBg.style.opacity = 1;
     aboutBg.style.opacity = 0;
@@ -23,5 +25,12 @@ window.addEventListener('scroll', function() {
     colorLinks.forEach(link => {
       link.style.color = '#333';
     });
+    aboutBg.classList.remove('visible');
+  }
+  if (window.scrollY > scrollTrigger2) {
+    aboutContent.classList.add('visible');
+  } else {
+    aboutContent.classList.remove('visible');
   }
 });
+
